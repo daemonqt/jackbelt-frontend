7c93c8463c967f0cb3900571e1a9a5dacaa82721
Paulo Lascano <smithlascano1@gmail.com>
2024-04-17 12:27:50 +0800

    add customer count

--- backend/app/routes/customerRoutes.js
+++ backend/app/routes/customerRoutes.js
@@ -42,6 +42,23 @@ router.get('/customers', authenticateToken, async (req, res) => {
     }
 });
 
+router.get('/customers/count', authenticateToken, async (req, res) => {
+    try {
+        db.query('SELECT COUNT(*) AS customerCount FROM customers', (err, result) => {
+            if (err) {
+                console.error('Error fetching customer count:', err);
+                res.status(500).json({ message: 'Internal Server Error' });
+            } else {
+                const customerCount = result[0].customerCount;
+                res.status(200).json({ customerCount });
+            }
+        });
+    } catch (error) {
+        console.error('Error loading customer count:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 router.get('/customer/:id', authenticateToken, async (req, res) => {
     
     let customer_id = req.params.id;
